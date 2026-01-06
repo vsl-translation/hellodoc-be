a84f9fdf31e8c51f322d27a1abf535c89f9993f0
vuphuong1794 <phuongvu1794@gmail.com>
2026-01-06 18:18:01 +0700

    update controller

--- apps/api-gateway/src/controller/sign_language.controller.ts
+++ apps/api-gateway/src/controller/sign_language.controller.ts
@@ -3,16 +3,16 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Response } from 'express';
 import { SignLanguageService } from '../services/sign_language.service';
 
-@Controller('gesture-code')
+@Controller('gesture_code')
 export class SignLanguageController {
   constructor(private readonly signLanguageService: SignLanguageService) { }
 
-  @Post('post-video-url')
+  @Post('post_video_url')
   async postVideoUrl(@Body() urlMedia: string) {
     return this.signLanguageService.postVideoUrl(urlMedia);
   }
 
-  @Get('get-gesture_code')
+  @Get('get_gesture_code')
   async getGestureWordCode(@Body() videoUrl: string) {
     return this.signLanguageService.getGestureWordCode(videoUrl);
   }
